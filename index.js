6336fae137cc1d6ae92fe3b62db4877aab016c42
Zidannn10 <zidanazzahra916@gmail.com>
2022-04-04 11:54:23 +0700

    akwwk

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require("dotenv").config();
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
+
+mongoose.connect('mongodb://localhost:27017/auth-app');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors())
+
+app.get("/", (req,res)=>{
+    res.send("hello jwt");
+});
+
+app.use("/customers", require("./routes/customerRoutes"))
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
